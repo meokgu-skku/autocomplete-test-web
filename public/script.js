0c093fa297b5432951bf0a356facc36a8ec9d8f0
sinkyoungdeok <tkagmd1@naver.com>
2024-05-11 15:52:04 +0900

    [KAN-50] es 검색 퀄리티를 높이기 위해 가중치를 반영한다

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var searchInput = document.getElementById('searchInput');
   var resultsContainer = document.getElementById('autocompleteResults');
 
-  searchInput.addEventListener('input', function() {
+  searchInput.addEventListener('input', function () {
     var query = this.value;
     fetchAutocompleteResults(query);
   });
 
-  searchInput.addEventListener('keypress', function(e) {
+  searchInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
       searchRestaurants(this.value);  // Function call to search restaurants using Elasticsearch
     }
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function displayResults(results) {
     resultsContainer.innerHTML = '';
-    results.forEach(function(item) {
+    results.forEach(function (item) {
       var div = document.createElement('div');
       div.className = 'autocomplete-entry'; // Added class for styling
       div.innerHTML = `${item.highlighted_display} <span class="category">${item.category}</span>`; // Display category
@@ -40,17 +40,17 @@ document.addEventListener('DOMContentLoaded', function() {
         query: {
           bool: {
             should: [
-              { match: { category: query }},
-              { match: { name: query }},
-              { match: { custom_category: query }},
+              {match: {name: {query: query, boost: 10}}},
+              {match: {category: {query: query, boost: 3}}},
+              {match: {custom_category: {query: query, boost: 3}}},
               {
                 nested: {
                   path: "menus",
                   query: {
                     bool: {
                       should: [
-                        { match: { "menus.menu_name": query }},
-                        { match: { "menus.description": query }}
+                        {match: {"menus.menu_name": {query: query, boost: 1}}},
+                        {match: {"menus.description": {query: query, boost: 1}}}
                       ]
                     }
                   }
@@ -63,32 +63,33 @@ document.addEventListener('DOMContentLoaded', function() {
       })
     })
     .then(response => response.json())
-    .then(data => displaySearchResults(data.hits.hits))  // Assuming response format
+    .then(data => displaySearchResults(
+        data.hits.hits))  // Assuming response format
     .catch(error => console.error('Error:', error));
   }
 
   function displaySearchResults(results) {
     var searchResultsContainer = document.getElementById('searchResults');
     searchResultsContainer.innerHTML = '';
-    results.forEach(function(item) {
+    results.forEach(function (item) {
       var div = document.createElement('div');
       div.className = 'search-result-entry';
-  
+
       var imageUrl = item._source.image_url || 'path/to/default-image.jpg'; // 기본 이미지 경로 추가
       var imageElement = document.createElement('img');
       imageElement.src = imageUrl;
-  
+
       var content = document.createElement('div');
       content.innerHTML = `<strong>${item._source.name}</strong><br/>
                            Category: ${item._source.category}<br/>
                            Address: ${item._source.address}<br/>
                            Rating: ${item._source.rating}<br/>`;
-  
+
       var menuInfo = document.createElement('div');
       menuInfo.className = 'menu-info';
-  
+
       if (item._source.menus) {
-        item._source.menus.forEach(function(menu) {
+        item._source.menus.forEach(function (menu) {
           var menuItem = document.createElement('div');
           menuItem.className = 'menu-item';
           var menuImage = document.createElement('img');
@@ -100,13 +101,12 @@ document.addEventListener('DOMContentLoaded', function() {
           menuInfo.appendChild(menuItem);
         });
       }
-  
+
       div.appendChild(imageElement);
       div.appendChild(content);
       div.appendChild(menuInfo);
       searchResultsContainer.appendChild(div);
     });
   }
-  
-  
+
 });
