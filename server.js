71b264816fd243dcb93c609738c5e3fd21d545ca
Bob Sin <tkagmd1@naver.com>
2024-05-11 14:20:45 +0900

    [KAN-49] 검색 어드민 페이지 활성화 (#2)

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 정적 파일 제공을 위한 미들웨어 설정
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
